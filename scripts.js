425575c4360e88a454452a982257ec786b1c6c4b
DanielMachado-web <danielmachadojhs@gmail.com>
2025-06-15 15:49:49 -0300

    alteração no projeto

--- scripts.js
+++ scripts.js
@@ -1,7 +1,42 @@
 const convertButton = document.querySelector(".convert-Button");
+const selectMoeda = document.querySelector(".select-moeda");
+
 
 function convertvalue() {
     const inputValue = document.querySelector(".input-value").value;
-    console.log(inputValue);
-   
-convertButton.addEventListener("click", convertvalue ) 
+    const moedavalorconverter = document.querySelector(".moeda-valor-converter"); //valor em real
+    const moedavalorConvertido = document.querySelector(".moeda-valor"); //outros valores
+
+    console.log(selectMoeda.value);
+    
+    const dolarToday = 5.2;
+    const euroToday = 6.2;
+
+    const convertedValue = inputValue / dolarToday;
+
+    moedavalorconverter.innerHTML = new Intl.NumberFormat("pt-BR", { // Formato brasileiro
+        style: "currency",
+        currency: "BRL"
+    }).format(inputValue);
+
+    moedavalorConvertido.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    }).format(convertedValue);
+
+    console.log(convertedValue);
+}
+convertButton.addEventListener("click", convertvalue);
+// primeira coisa que o usuario vai fazer é clicar no botao de converter, entao vamos adicionar o evento de click no botao//
+
+
+
+
+
+
+
+
+
+
+
+
